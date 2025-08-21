72318535b94ed5713053804f355849aa57f8656e
Runeword <60324746+Runeword@users.noreply.github.com>
2025-08-22 00:10:14 +0200

    Add nix/flake.nix

---
+++ nix/flake.nix
@@ -0,0 +1,17 @@
+{
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+  outputs = {self, ...} @ inputs: let
+    pkgs = import inputs.nixpkgs {
+      system = "x86_64-linux";
+    };
+  in {
+    devShells.x86_64-linux = {
+      default = pkgs.mkShell {
+        packages = [
+          pkgs.go
+        ];
+      };
+    };
+  };
+}
