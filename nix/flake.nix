51056cceacb56f1c2e9fe15fecc6494d6a6d2450
Runeword <60324746+Runeword@users.noreply.github.com>
2025-08-23 22:37:24 +0200

    Update nix/flake.nix

--- nix/flake.nix
+++ nix/flake.nix
@@ -1,17 +1,24 @@
 {
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-  outputs = {self, ...} @ inputs: let
-    pkgs = import inputs.nixpkgs {
-      system = "x86_64-linux";
-    };
-  in {
-    devShells.x86_64-linux = {
-      default = pkgs.mkShell {
-        packages = [
-          pkgs.go
-        ];
+  outputs =
+    { self, ... }@inputs:
+    let
+      pkgs = import inputs.nixpkgs {
+        system = "x86_64-linux";
+      };
+    in
+    {
+      devShells.x86_64-linux = {
+        default = pkgs.mkShell {
+          packages = [
+            pkgs.go
+            pkgs.go-task
+          ];
+          shellHook = ''
+            echo "tw, tk"
+          '';
+        };
       };
     };
-  };
 }
